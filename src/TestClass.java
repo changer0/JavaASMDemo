afbf31222c33bcbdd7427c1dc12d427f96b1ed36
p_zlulzhang <zhanglulu@yuewen.com>
2020-05-26 20:08:02 +0800

    update

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,6 +3,7 @@ public class TestClass {
         new TestClass().newFun();
     }
     private long funA(int n, String s, int[] arr) {
+        System.out.println("funA");
         return 0;
     }
 
@@ -10,5 +11,18 @@ public class TestClass {
         //System.out.println("before insert");
         System.out.println("running");
         //System.out.println("after insert");
+
+        fun("a");
+    }
+
+    private void fun(String a) {
+
+    }
+
+    public void sample1(){
+        int num = 5;
+    }
+    public int sample2(int a, int b) {
+        return a + b;
     }
 }
